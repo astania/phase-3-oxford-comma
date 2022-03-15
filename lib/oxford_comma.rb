faad0c219a76a38a412029743181c3143d202ec4
+ <+>
2022-03-15 12:00:21 -0400

    commit tree 74ca1b73b9222526503c9f79efba9f04a8536c29

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+    if array.length < 2 
+        array.join
+    elsif array.length == 2
+        array.join(" and ")
+    elsif array.length > 2
+        new_array = array.map{|word| "#{word},"}
+        new_array[-1] = "and #{array[-1]}"
+        new_array.join(" ")
+    end
+    
+    
 end
